616810ac602e4778b75f145d8cf91f17b6e6746f
svetlichnaya.org@gmail.com <svetlichnaya.org@gmail.com>
2024-03-02 15:08:32 +0100

    adding footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,178 @@
+import { useState } from "react";
 import "./Footer.scss";
+import { Link } from "react-router-dom";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 export const Footer: React.FC = () => {
-  return <footer className="footer">footer</footer>;
+  // State to manage which accordion item is open, -1 means all are closed
+  const [openAccordion, setOpenAccordion] = useState<number>(-1);
+
+  // Toggle function to open/close accordion items
+  const toggleAccordion = (index: number) => {
+    if (openAccordion === index) {
+      // If clicked again on the same item, close it
+      setOpenAccordion(-1);
+    } else {
+      // Open the clicked item
+      setOpenAccordion(index);
+    }
+  };
+
+  return(
+    <div className="home__container" style={{ paddingInline: "0px" }}>
+      <footer className="footer__main">
+        <div className="footer__newsletter">
+          <p>Sign up to receive the most interesting and favourable rental offers every week!</p>
+          <form className="footer__newsletter-input-group">
+            <input type="email" placeholder="Enter your Email" />
+            <button type="submit"><span>Subscribe</span></button>
+          </form>
+        </div>
+
+        <div className="footer__content">
+          <div className="footer__content-part-one">
+            <div className="footer__content-section-one">
+              <div className="footer__logo-div">
+                <Link to="/" className="footer__logo">
+                  Logo
+                </Link>
+              </div>
+              <div className="footer__logo-info-div">
+                <p className="footer__text-info">Technical support</p>
+                <p className="footer__text-tel">0 800 300 501</p>
+                <p className="footer__text-info">Email:</p>
+                <p className="footer__text-info">info@homey.com</p>
+              </div>
+            </div>
+
+            <div className="footer__content-section-two">
+              <div className="footer__section-categories-section">
+                <p className="footer__categories">Categories</p>
+                <ul>
+                  <li><a href="#">Houses</a></li>
+                  <li><a href="#">Flats</a></li>
+                  <li><a href="#">Hotels</a></li>
+                  <li><a href="#">Hostels</a></li>
+                </ul>
+              </div>
+              <div className="footer__section-support-section">
+                <p className="footer__categories">Support</p>
+                <ul>
+                  <li><a href="#">FAQ`s</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                  <li><a href="#">Return Policy</a></li>
+                  <li><a href="#">My Account</a></li>
+                </ul>
+              </div>
+              <div className="footer__section-social-section">
+                <p className="footer__categories">Social</p>
+                <ul>
+                  <li><a href="#">Facebook</a></li>
+                  <li><a href="#">Instagram</a></li>
+                  <li><a href="#">Pinterest</a></li>
+                  <li><a href="#">YouTube</a></li>
+                </ul>
+              </div>
+              <div className="footer__section-legal-section">
+                <p className="footer__categories">Legal</p>
+                <ul>
+                  <li><a href="#">Privacy Policy</a></li>
+                  <li><a href="#">Terms of Use</a></li>
+                  <li><a href="#">Frequently Asked Questions</a></li>
+                  <li><a href="#">Cancellation Policy</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="footer__accordion-sections">
+            <div className="footer__accordion-item">
+              <div className="footer__accordion-title" onClick={() => toggleAccordion(1)}>
+                <p className="footer__categories">Categories</p>
+                {openAccordion === 1 ? <IoIosArrowUp /> : <IoIosArrowDown />}
+              </div>
+              {openAccordion === 1 && (
+                <div className="footer__accordion-content">
+                  <ul>
+                    <li><a href="#">Houses</a></li>
+                    <li><a href="#">Flats</a></li>
+                    <li><a href="#">Hotels</a></li>
+                    <li><a href="#">Hostels</a></li>
+                  </ul>
+                </div>
+              )}
+            </div>
+            
+            <div className="footer__accordion-item">
+              <div className="footer__accordion-title" onClick={() => toggleAccordion(2)}>
+                <p className="footer__categories">Support</p>
+                {openAccordion === 2 ? <IoIosArrowUp /> : <IoIosArrowDown />}
+              </div>
+              {openAccordion === 2 && (
+                <div className="footer__accordion-content">
+                  <ul>
+                    <li><a href="#">FAQ`s</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Return Policy</a></li>
+                    <li><a href="#">My Account</a></li>
+                  </ul>
+                </div>
+              )}
+            </div>
+
+            <div className="footer__accordion-item">
+              <div className="footer__accordion-title" onClick={() => toggleAccordion(3)}>
+                <p className="footer__categories">Social</p>
+                {openAccordion === 3 ? <IoIosArrowUp /> : <IoIosArrowDown />}
+              </div>
+              {openAccordion === 3 && (
+                <div className="footer__accordion-content">
+                  <ul>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Pinterest</a></li>
+                    <li><a href="#">YouTube</a></li>
+                  </ul>
+                </div>
+              )}
+            </div>
+
+            <div className="footer__accordion-item">
+              <div className="footer__accordion-title" onClick={() => toggleAccordion(4)}>
+                <p className="footer__categories">Legal</p>
+                {openAccordion === 4 ? <IoIosArrowUp /> : <IoIosArrowDown />}
+              </div>
+              {openAccordion === 4 && (
+                <div className="footer__accordion-content">
+                  <ul>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms of Use</a></li>
+                    <li><a href="#">Frequently Asked Questions</a></li>
+                    <li><a href="#">Cancellation Policy</a></li>
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div> 
+
+          <div className="footer__newsletter_mobil">
+            <p className="footer__categories">Service:</p>
+            <p id="tel" className="footer__categories">0 800 300 501</p>
+          </div>
+
+          <div className="footer__newsletter-info-mobil">
+            <p className="footer__text-info">Email</p>
+            <p id="email" className="footer__text-info">info@homey.com</p>   
+          </div>
+
+          <div className="footer__content_part_two_main">
+            <div className="footer__content-part-two">
+              <p id="page" className="footer__text-info">&copy;2024 Homey.com</p>
+            </div>
+          </div>
+          
+        </div>
+      </footer>
+    </div>
+  );
 };
