1006abcdf72439793a5f50545afa47b7b24ff9d6
svetlichnaya.org@gmail.com <svetlichnaya.org@gmail.com>
2024-03-17 16:05:17 +0100

    put constants from About to contants order

---
+++ src/pages/About/constants/index.ts
@@ -0,0 +1,73 @@
+import Rectangle4 from "@/assets/images/Rectangle4.png";
+
+export const teamMembers = [
+    {
+        id: 1,
+        name: "James Wilson",
+        role: "Customer service manager",
+        description:
+            "He helps customers find and book accommodations, resolves any questions and problems related to stay, and offers special offers and discounts. He has over 6 years of experience in customer service and sales.",
+        imageUrl: Rectangle4,
+    },
+    {
+        id: 2,
+        name: "Emma Johnson",
+        role: "Marketing and advertising specialist",
+        description:
+          "She develops and implements marketing strategies and campaigns and is responsible for promoting our website and app on the internet and social media. She has more than 5 years of experience in marketing and advertising.",
+        imageUrl: Rectangle4,
+    },
+    {
+        id: 3,
+        name: "David Smith",
+        role: "Web Developer and Designer",
+        description:
+          "He builds and maintains our website and app, and implements new technologies and features. He has over 8 years of experience in web design and programming.",
+        imageUrl: Rectangle4,
+    },
+    {
+        id: 4,
+        name: "Sophia Jones",
+        role: "Accountant and Financial Officer",
+        description:
+          "She maintains records and accounts for our company's financial transactions and monitors expenses and revenues. She has over 7 years of experience in accounting and finance.",
+        imageUrl: Rectangle4,
+    },
+    {
+        id: 5,
+        name: "Michael Brown",
+        role: "Lawyer and counsellor",
+        description:
+          "He advises our company on legal issues related to tenancy and defends our rights and interests in case of disputes or conflicts. He has over 9 years of legal and arbitration experience.",
+        imageUrl: Rectangle4,
+    },
+    {
+        id: 6,
+        name: "Alexander Garcia",
+        role: "Analyst and market researcher",
+        description:
+          "He researches the rental market and abroad, analyses the needs and behaviour of clients, and evaluates the effectiveness and results of our service. He has over 5 years of experience in analytics and research.",
+        imageUrl: Rectangle4,
+    },
+];
+
+export const cardsData = [
+    {
+      id: 1,
+      title: "Over 1,000 happy customers",
+      description: "Over 1,000 happy customers who have found the perfect home through our company.",
+      imageUrl: Rectangle4
+    },
+    {
+      id: 2,
+      title: "5-star Google Reviews",
+      description: "Our company has consistently received 5-star ratings on Google Reviews.",
+      imageUrl: Rectangle4
+    },
+    {
+      id: 3,
+      title: "Award-Winning Accommodation",
+      description: "Helped rent accommodation for a film that won an award at an international film festival.",
+      imageUrl: Rectangle4
+    }
+];
